b66b6d36bd30e47975606dddd72ced4b4430bc63
Grace Sun <gtsun@email.unc.edu>
2022-11-30 21:49:17 -0500

    added parseInt

--- server.js
+++ server.js
@@ -11,31 +11,33 @@ const port = args.port || 5000;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
-
-
 //Check endpoint at /app/ that returns 200 OK.
 app.get('/app/', (req, res) => {
     res.send('200 OK').end();
 })
-
 
 //Endpoint /app/roll/ that returns JSON for a default roll of two six-sided dice one time.
 app.get('/app/roll/', (req, res) => {
     res.send(roll(6, 2, 1)).end();
 })
 
-
 //Endpoint /app/roll/ should ALSO accept either JSON or URLEncoded data body for sides, dice, and rolls. 
-//Example URLEncoded string for data body: ?sides=20&dice=4&rolls=3. Example JSON data body: {"sides":20,"dice":4,"rolls":3}. 
-//The format of the resulting JSON should look like: {"sides":20,"dice":4,"rolls":3,"results":[19,3,60]}.
 
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(req.query.sides, req.query.dice, req.query.rolls)).end();
+    res.send(roll(parseInt(req.query.sides), parseInt(req.query.dice), parseInt(req.query.rolls))).end();
 })
 
 app.post('/app/roll/', (req, res) => {
-    res.send(roll(req.body.sides, req.body.dice, req.body.rolls)).end();
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))).end();
 })
+
+//Endpoint /app/roll/:sides/ that returns JSON for a default number of rolls and dice with whatever number of sides is specified in the parameter. 
+//For example, /app/roll/6/ should return JSON for two six-sided dice, rolled one time, whereas /app/roll/10/ should return JSON for two ten-sided dice, rolled 1 time. 
+//The format of the resulting JSON should look like: {"sides":10,"dice":2,"rolls":1,"results":[17]}.
+
+//app.get('/app/roll/:sides/', (req, res) => {
+ //   res.send(roll(, 2, 1)).end();
+//})
 
 //Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined.
 app.get('*', (req, res) => {
