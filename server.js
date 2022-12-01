2cd5338d60c80c5bf6379e8fcd39a0e520870b98
Grace Sun <gtsun@email.unc.edu>
2022-11-30 20:29:54 -0500

    update server.js

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ const port = args.port || 5000;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
+
+
+//Check endpoint at /app/ that returns 200 OK.
+app.get('/app/', (req, res) => {
+    res.send('200 OK').end();
+})
+
+
+
+
+
+
+//Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined.
 app.get('*', (req, res) => {
     res.send('404 NOT FOUND').end()
 })
