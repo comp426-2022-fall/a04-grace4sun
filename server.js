de57202e63fa12eb972c6f95d27c63260e2c1708
Grace Sun <gtsun@email.unc.edu>
2022-11-30 20:27:34 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
 import { roll } from './lib/roll.js';
 import minimist from 'minimist';
 import express from 'express';
+
+//command line arguments
+const args = minmist(process.arg.slice(2));
+const port = args.port || 5000;
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+app.get('*', (req, res) => {
+    res.send('404 NOT FOUND').end()
+})
+
+app.listen(port);
