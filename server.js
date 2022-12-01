797dacb3fed4e010dc3c2c842e477f0edea2b9e7
Grace Sun <gtsun@email.unc.edu>
2022-11-30 22:14:04 -0500

    added headers and status

--- server.js
+++ server.js
@@ -18,35 +18,38 @@ app.get('/app/', (req, res) => {
 
 //Endpoint /app/roll/ that returns JSON for a default roll of two six-sided dice one time.
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(6, 2, 1)).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(6, 2, 1)).end();
 })
 
 //Endpoint /app/roll/ should ALSO accept either JSON or URLEncoded data body for sides, dice, and rolls. 
-
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(parseInt(req.query.sides), parseInt(req.query.dice), parseInt(req.query.rolls))).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(parseInt(req.query.sides), parseInt(req.query.dice), parseInt(req.query.rolls))).end();
 })
 
 app.post('/app/roll/', (req, res) => {
-    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))).end();
 })
 
 //Endpoint /app/roll/:sides/ that returns JSON for a default number of rolls and dice with whatever number of sides is specified in the parameter. 
 
 app.get('/app/roll/:sides/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), 2, 1)).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(parseInt(req.params.sides), 2, 1)).end();
 })
 
 //Endpoint /app/roll/:sides/:dice/ that returns JSON for a default number of rolls with whatever number of sides and dice specified in the parameters. 
-
 app.get('/app/roll/:sides/:dice/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)).end();
 })
 
 //Endpoint /app/roll/:sides/:dice/:rolls/ that returns JSON for the specified number of rolls with whatever number of sides and dice specified in the parameters.
-
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))).end();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))).end();
 })
 
 //Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined.
